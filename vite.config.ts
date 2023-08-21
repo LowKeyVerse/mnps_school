497bc24b5ded5ef99194d01e87e0c629ec084be2
loki-22-07 <shamsher.lokesh@gmail.com>
2023-08-21 12:03:21 +0530

    deploy with vite configs updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "mnps_school",
+  base: "/mnps_school/",
   plugins: [react()],
 });
